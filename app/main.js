7dd1834af96198f5d48bf0ae3dd937fe0138413e
Sayam Alvi <sayamalvi07@gmail.com>
2024-08-22 19:49:47 +0530

    init basic commands

---
+++ app/main.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+
+const command = process.argv[2];
+const GitClient = require("./git/client");
+const gitClient = new GitClient();
+
+const { CatFileCommand } = require("./git/commands/index");
+
+switch (command) {
+  case "init":
+    createGitDirectory();
+    break;
+  case "cat-file":
+    handleCatFileCommand();
+    break;
+  default:
+    throw new Error(`Unknown command ${command}`);
+}
+
+function createGitDirectory() {
+  fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
+  fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), {
+    recursive: true,
+  });
+  fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
+
+  fs.writeFileSync(
+    path.join(process.cwd(), ".git", "HEAD"),
+    "ref: refs/heads/main\n"
+  );
+  console.log("Initialized git directory");
+}
+function handleCatFileCommand() {
+  const flag = process.argv[3];
+  const commitSHA = process.argv[4];
+  const command = new CatFileCommand(flag, commitSHA);
+  gitClient.run(command);
+}
